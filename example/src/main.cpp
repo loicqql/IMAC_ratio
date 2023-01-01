29a5fb2f67ceabf8edb6c50a103a7d8087cbe34a
loicqql <loic.qql@gmail.com>
2023-01-01 18:37:12 +0100

    example file

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,44 +1,159 @@
+/*
+ ________   ________   _________   ___   ________     
+|\   __  \ |\   __  \ |\___   ___\|\  \ |\   __  \    
+\ \  \|\  \\ \  \|\  \\|___ \  \_|\ \  \\ \  \|\  \   
+ \ \   _  _\\ \   __  \    \ \  \  \ \  \\ \  \\\  \  
+  \ \  \\  \|\ \  \ \  \    \ \  \  \ \  \\ \  \\\  \ 
+   \ \__\\ _\ \ \__\ \__\    \ \__\  \ \__\\ \_______\
+    \|__|\|__| \|__|\|__|     \|__|   \|__| \|_______|
+
+*/ 
+
 #include <iostream>
 
 #include "Ratio.hpp"
 
+//https://stackoverflow.com/questions/29997096/bold-output-in-c
+class bold {
+    std::string_view const &s;
+public:
+    bold(std::string_view const &s) : s(s) {}
+
+    friend std::ostream &operator<<(std::ostream &os, bold const &b) {
+        os << "\x1b[7m" << b.s << "\x1b[0m";
+        return os;
+    }
+};
+
+void title();
+
 
 int main() {
 
-    //contructors
-    Ratio rat(3, 2);
-	Ratio rat2(1, 2);
+    title();
 
-    //osstream
-    std::cout << "ratio : " << rat << std::endl;
+    std::cout << "\x1b[1m" << bold("--EXAMPLES--") << std::endl << std::endl;
 
-    //operator *
-    std::cout << rat << " * " << rat2 << " = " << rat * rat2 << std::endl;
+    /////////////////////////////////////////////////////
+    // constructors
+    {
+        std::cout << bold("-constructors") << std::endl;
+        std::cout << "Ratio rat(1,2) = " << Ratio(1,2) << std::endl;
+        std::cout << "Ratio rat(3.5) = " << Ratio(3.5) << std::endl;
+        std::cout << "Ratio rat(4) = " << Ratio(4) << std::endl;
+        std::cout << std::endl;
+    }
 
-    //operator +
-    std::cout << rat << " + " << rat2 << " = " << rat + rat2 << std::endl;
+    /////////////////////////////////////////////////////
+    // inverse
+    {
+        std::cout << bold("-inverse") << std::endl;
+        Ratio rat(1,2);
+        rat.inverse();
+        std::cout << "Ratio rat(1,2)" << std::endl;
+        std::cout << "rat.inverse() = " << rat << std::endl;
+        std::cout << std::endl;
+    }
 
-    //operator -
-    std::cout << rat << " - " << rat2 << " = " << rat - rat2 << std::endl;
+    /////////////////////////////////////////////////////
+    // operators
+    {
+        std::cout << bold("-operators") << std::endl;
+        Ratio rat(0.5);
+        Ratio rat2(3.5);
+        std::cout << "Ratio rat(0.5)" << std::endl;
+        std::cout << "Ratio rat2(3.5)" << std::endl;
+        std::cout << "rat + rat2 = " << rat + rat2 << std::endl;
+        std::cout << "rat - rat2 = " << rat - rat2 << std::endl;
+        std::cout << "rat * rat2 = " << rat * rat2 << std::endl;
+        std::cout << "rat / rat2 = " << rat / rat2 << std::endl;
+        std::cout << std::endl;
+    }
 
-    Ratio rat5(2);
-    std::cout << rat5 << std::endl;
+    /////////////////////////////////////////////////////
+    // copy assignment
+    {
+        std::cout << bold("-copy assignment") << std::endl;
+        Ratio rat(0.5);
+        Ratio rat2 = rat;
+        std::cout << "Ratio rat(0.5)" << std::endl;
+        std::cout << "Ratio rat2 = rat" << std::endl;
+        std::cout << "rat2 = " << rat2 << std::endl;
+        std::cout << std::endl;
+    }
 
-    //floor et abs
-    Ratio rat6(-1.5);
-    std::cout << rat6 << std::endl;
-    rat6=Ratio::floor(rat6);
-    std::cout << rat6 << std::endl;
-    rat6=Ratio::abs(rat6);
-    std::cout << rat6 << std::endl;
+    /////////////////////////////////////////////////////
+    // operations with number
+    {
+        std::cout << bold("-operations with number") << std::endl;
+        Ratio rat(0.5);
+        std::cout << "Ratio rat(0.5)" << std::endl;
+        std::cout << "rat + 2 = " << rat + 2 << std::endl;
+        std::cout << "rat - 2 = " << rat - 2 << std::endl;
+        std::cout << "rat * 2 = " << rat * 2 << std::endl;
+        std::cout << "rat / 2 = " << rat / 2 << std::endl;
+        std::cout << std::endl;
+    }
 
-    Ratio ratPow(2,5);
-    ratPow=Ratio::pow<int>(ratPow,-2);
-    std::cout << ratPow << std::endl;
+    /////////////////////////////////////////////////////
+    // unary minus
+    {
+        std::cout << bold("-unary minus") << std::endl;
+        Ratio rat(0.5);
+        std::cout << "Ratio rat(0.5)" << std::endl;
+        std::cout << "-rat = " << -rat<< std::endl;
+        std::cout << std::endl;
+    }
 
-    Ratio ratSqrt(9,25);
-    ratSqrt=Ratio::sqrt(ratSqrt);
-    std::cout << ratSqrt << std::endl;
+    /////////////////////////////////////////////////////
+    // comparison functions
+    {
+        std::cout << bold("-comparison functions") << std::endl;
+        Ratio rat(0.5);
+        Ratio rat2(2.5);
+        std::cout << "Ratio rat(0.5)" << std::endl;
+        std::cout << "Ratio rat2(2.5)" << std::endl;
+        std::cout << "rat < rat2 = " << (rat < rat2 ? "true" : "false") << std::endl;
+        std::cout << "rat > rat2 = " << (rat > rat2 ? "true" : "false") << std::endl;
+        std::cout << "rat <= rat2 = " << (rat <= rat2 ? "true" : "false") << std::endl;
+        std::cout << "rat >= rat2 = " << (rat >= rat2 ? "true" : "false") << std::endl;
+        std::cout << "rat == rat2 = " << (rat == rat2 ? "true" : "false") << std::endl;
+        std::cout << "rat != rat2 = " << (rat != rat2 ? "true" : "false") << std::endl;
+        std::cout << std::endl;
+    }                                                                               
+
+    /////////////////////////////////////////////////////
+    // mathematical functions
+    {
+        std::cout << bold("-mathematical functions") << std::endl;
+        Ratio rat(0.5);
+        std::cout << "Ratio rat(0.5)" << std::endl;
+        std::cout << "Ratio::abs(-rat) = " << Ratio::abs(-rat) << std::endl;
+        std::cout << "Ratio::floor(rat) = " << Ratio::floor(rat) << std::endl;
+        std::cout << "Ratio::sin(rat) = " << Ratio::sin(rat) << " = " << (double)Ratio::sin(rat).numerator() /  Ratio::sin(rat).denominator() << " (not recommended)" << std::endl;
+        std::cout << "Ratio::cos(rat) = " << Ratio::cos(rat) << " = " << (double)Ratio::cos(rat).numerator() /  Ratio::cos(rat).denominator() << " (not recommended)" << std::endl;
+        std::cout << "Ratio::tan(rat) = " << Ratio::tan(rat) << " = " << (double)Ratio::tan(rat).numerator() /  Ratio::tan(rat).denominator() << " (not recommended)" << std::endl;
+        std::cout << "Ratio::log(rat) = " << Ratio::log(rat) << " = " << (double)Ratio::log(rat).numerator() /  Ratio::log(rat).denominator() << " (not recommended)" << std::endl;
+        std::cout << "Ratio::sqrt(rat) = " << Ratio::sqrt(rat) << std::endl;
+        std::cout << std::endl;
+    }
 
     return 0;
 }
+
+void title() {
+    printf(R"EOF(
+ ________   ________   _________   ___   ________     
+|\   __  \ |\   __  \ |\___   ___\|\  \ |\   __  \    
+\ \  \|\  \\ \  \|\  \\|___ \  \_|\ \  \\ \  \|\  \   
+ \ \   _  _\\ \   __  \    \ \  \  \ \  \\ \  \\\  \  
+  \ \  \\  \|\ \  \ \  \    \ \  \  \ \  \\ \  \\\  \ 
+   \ \__\\ _\ \ \__\ \__\    \ \__\  \ \__\\ \_______\
+    \|__|\|__| \|__|\|__|     \|__|   \|__| \|_______|
+    )EOF");
+
+    std::cout << std::endl << std::endl;
+}
+
+
+//0101001001100001011101000110100101101111 0011101000101001
